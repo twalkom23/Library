5d442853f5f5ade376e12cb7c0d8295a3e5f6eb7
Tim Walkom <timwalkom@hotmail.com>
2024-01-31 16:10:04 +1030

    Created basic structure for webpage

---
+++ myScript.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function bookObject(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+}
+

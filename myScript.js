99d12746d22d85a16f599180b8d947cf4a6d93c9
Tim Walkom <timwalkom@hotmail.com>
2024-02-06 18:17:31 +1030

    The inputs now get logged on the screen

--- myScript.js
+++ myScript.js
@@ -3,9 +3,13 @@ let author;
 let pages;
 let read;
 
+const titleLog = document.querySelector('.title');
+const authorLog = document.querySelector('.author');
+const pagesLog = document.querySelector('.pages');
+const readLog = document.querySelector('.readYN');
 
 const myLibrary = [];
-
+//new book constructor to be used later
 function bookObject(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
@@ -29,6 +33,40 @@ function readOrNot() {
         return ' ';
 }
 
+//This function will go through the array and display the books on the page
+function libraryDisplay(array) {
+    titleLog.innerHTML = '';
+    authorLog.innerHTML = '';
+    pagesLog.innerHTML = '';
+    readLog.innerHTML = '';
+    for (let i = 0; i < array.length; i++){
+        //adding title to DOM
+        const contentTitle = document.createElement('div');
+        contentTitle.classList.add('contentTitle');
+        contentTitle.textContent = myLibrary[i].title;
+        titleLog.appendChild(contentTitle);
+
+        //adding author to DOM
+        const contentAuthor = document.createElement('div');
+        contentAuthor.classList.add('contentAuthor');
+        contentAuthor.textContent = myLibrary[i].author;
+        authorLog.appendChild(contentAuthor);
+
+        //adding pages to DOM
+        const contentPages = document.createElement('div');
+        contentPages.classList.add('contentPages');
+        contentPages.textContent = myLibrary[i].pages;
+        pagesLog.appendChild(contentPages);
+
+        //adding read or not to DOM
+        const contentRead = document.createElement('div');
+        contentRead.classList.add('contentRead');
+        contentRead.textContent = myLibrary[i].haveRead;
+        readLog.appendChild(contentRead);
+    }
+}
+
+
 
 
 
@@ -39,12 +77,11 @@ document.getElementById('libForm').addEventListener('submit', function(e) {
     author = document.getElementById('authorForm').value;
     pages = document.getElementById('pagesForm').value;
     read = readOrNot();
+    const newBook = new bookObject(title, author, pages, read);
+    myLibrary.push(newBook);
+    libraryDisplay(myLibrary);
+    
     clearForm();
-
-    console.log(title);
-    console.log(author);
-    console.log(pages);
-    console.log(read);
 })
 
 

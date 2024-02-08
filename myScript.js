6f4190f1348efedb4854ecb66da4d37471bab956
Tim Walkom <timwalkom@hotmail.com>
2024-02-08 19:17:47 +1030

    Added a button to toggle haveread variable

--- myScript.js
+++ myScript.js
@@ -2,6 +2,7 @@ let title;
 let author;
 let pages;
 let read;
+let delBTN;
 
 const titleLog = document.querySelector('.title');
 const authorLog = document.querySelector('.author');
@@ -11,6 +12,8 @@ const delLog = document.querySelector('.delete');
 
 const myLibrary = [];
 let delArray = [];
+let yesOrNoArray = [];
+
 //new book constructor to be used later
 function bookObject(title, author, pages, haveRead) {
     this.title = title;
@@ -35,7 +38,11 @@ function readOrNot() {
         return ' ';
 }
 //Function will delete a row when the delete button is clicked.
-function deleteRow() {
+function deleteRow(index) {
+    myLibrary.splice(index, 1);
+    delArray.splice(index, 1);
+
+    libraryDisplay(myLibrary);
 
 }
 
@@ -68,22 +75,51 @@ function libraryDisplay(array) {
         pagesLog.appendChild(contentPages);
 
         //adding read or not to DOM
-        const contentRead = document.createElement('div');
+        const contentRead = document.createElement('button');
         contentRead.classList.add('contentRead');
         contentRead.textContent = myLibrary[i].haveRead;
         readLog.appendChild(contentRead);
 
+
         //adding the delete option
-        const delBTN = document.createElement('button');
+        delBTN = document.createElement('button');
         delBTN.innerHTML = 'DEL';
         delLog.appendChild(delBTN);
+
+        
         //Putting the delete button in to an array
         while (delArray.length < array.length) {
             delArray.push(delBTN);
         }
+
+        delBTN.addEventListener('click', function() {
+            deleteRow(i);
+        })
+
+        contentRead.addEventListener('click', function() {
+                contentRead.classList.toggle('clicked');
+
+                setTimeout(function() {
+                    contentRead.classList.remove('clicked');
+                }, 100);
+               
+                let currentHaveRead = myLibrary[i].haveRead;
+                
+
+                if (currentHaveRead === 'Yes') {
+                        myLibrary[i].haveRead = 'No';
+                }
+                if (currentHaveRead === 'No') {
+                        myLibrary[i].haveRead = 'Yes';
+                }
+               contentRead.textContent = myLibrary[i].haveRead;
+               
+        })
+
         
         
     }
+    
     
 }
 
